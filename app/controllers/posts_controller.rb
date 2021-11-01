aa57f2e4d88fdf0cd4f07ac199ae8b39ef32eec4
shu <a1310shuki@gmail.com>
2021-11-01 15:20:03 +0900

    ○○.index.erbを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
+        @posts = Post.all.order(created_at: :desc)
         # .page(params[:page]).per(20)
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @posts = @user.posts 
+        @posts = @user.posts.order(created_at: :desc)
         # .page(params[:page]).per(10)
     end
 
